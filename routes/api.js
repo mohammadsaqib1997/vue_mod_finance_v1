bc364d792e485ba7e3147268e7af3978b1c2537b
Muhammad Saqib <mohammadsaqib1997@gmail.com>
2017-09-20 19:53:41 +0500

    All Reports complete

--- routes/api.js
+++ routes/api.js
@@ -18,6 +18,10 @@ var client = elasticSearch.Client({
 var nodemailer = require("nodemailer");
 var transporter = nodemailer.createTransport(config.smtp);
 
+router.get('/payment', function (req, res, next) {
+    res.render('pdf_templates/payment_plan');
+});
+
 router.post('/check_user', cors(), function (req, res, next) {
     req.assert('email', 'Email is required!').notEmpty();
     req.assert('password', 'Password is required!').notEmpty();
