b2b4a2ec4dc3e9f64ec9a8dc6c0ba97acdd28fe9
Muhammad Saqib <mohammadsaqib1997@gmail.com>
2017-10-16 18:58:41 +0500

    Complete pagination search in posts

--- src/pages/process/post_unpost/script.js
+++ src/pages/process/post_unpost/script.js
@@ -21,8 +21,10 @@ export default {
                     self.projectsRef.child(item.sel_project).once("value", function (proSnap) {
                         let proData = proSnap.val();
                         item['project'] = proData.name;
-
                         item['type'] = "jv";
+                        item['gen_type'] = "Journal Voucher";
+                        item['gen_date'] = self.formatDate(new Date(item.voucher_date));
+
                         if(item.ref_key !== ""){
                             if(item.ref_type === "pi"){
                                 self.partyInformationRef.child(item.ref_key).once('value', function (piSnap) {
@@ -33,6 +35,7 @@ export default {
                                     process_item++;
                                     if(process_item === snap.numChildren()){
                                         self.vouchersData = func.sortObj(self.vouchersData, false);
+                                        self.orgData = self.vouchersData;
                                         self.loadMasterDetails(self, function () {
                                             self.dataLoad1 = false;
                                         });
@@ -47,6 +50,7 @@ export default {
                                     process_item++;
                                     if(process_item === snap.numChildren()){
                                         self.vouchersData = func.sortObj(self.vouchersData, false);
+                                        self.orgData = self.vouchersData;
                                         self.loadMasterDetails(self, function () {
                                             self.dataLoad1 = false;
                                         });
@@ -57,6 +61,7 @@ export default {
                                 process_item++;
                                 if(process_item === snap.numChildren()){
                                     self.vouchersData = func.sortObj(self.vouchersData, false);
+                                    self.orgData = self.vouchersData;
                                     self.loadMasterDetails(self, function () {
                                         self.dataLoad1 = false;
                                     });
@@ -67,6 +72,7 @@ export default {
                             process_item++;
                             if(process_item === snap.numChildren()){
                                 self.vouchersData = func.sortObj(self.vouchersData, false);
+                                self.orgData = self.vouchersData;
                                 self.loadMasterDetails(self, function () {
                                     self.dataLoad1 = false;
                                 });
@@ -88,6 +94,15 @@ export default {
 
             //data
             vouchersData: {},
+            orgData: {},
+            search_txt: '',
+            pagData: {},
+
+            maxRows: 5,
+            totRows: 0,
+            totPages: 1,
+            curPage: 1,
+            start: 0,
 
             //references
             projectsRef: null,
@@ -96,6 +111,18 @@ export default {
             vouchersRef: null,
             projectTypeItemsRef: null,
             vouchersEntriesRef: null,
+        }
+    },
+    watch: {
+        search_txt: function (val) {
+            this.search_values(this, val);
+        },
+        vouchersData: function (val) {
+            this.pagination(this, val);
+        },
+        curPage: function (val) {
+            this.start = (val * this.maxRows) - (this.maxRows - 1);
+            this.changePage();
         }
     },
     methods: {
@@ -158,6 +185,8 @@ export default {
                             let proData = proSnap.val();
                             item['project'] = proData.name;
                             item['type'] = "md";
+                            item['gen_type'] = "Customer Details";
+                            item['gen_date'] = self.formatDate(new Date(item.booking_date));
 
                             self.projectTypeItemsRef.child(item.sel_pro_type_no).once('value', function (proSelItemSnap) {
                                 let data = proSelItemSnap.val();
@@ -176,6 +205,83 @@ export default {
                     callback();
                 }
             });
+        },
+        search_values: function (self, val) {
+            let saveData = self.orgData;
+            if(val !== ""){
+                let gen_search_data = {};
+                let searchKeys = Object.keys(saveData);
+                for(let i=0; i < searchKeys.length; i++){
+                    let sKey = searchKeys[i];
+                    let sItem = saveData[sKey];
+
+                    val = val.toLowerCase();
+
+                    if((sItem.id).toString().indexOf(val) > -1){
+                        gen_search_data[sKey] = sItem;
+                        continue;
+                    }
+                    if(sItem.gen_type.toLowerCase().indexOf(val) > -1){
+                        gen_search_data[sKey] = sItem;
+                        continue;
+                    }
+                    if(sItem.ref_gen.toLowerCase().indexOf(val) > -1){
+                        gen_search_data[sKey] = sItem;
+                        continue;
+                    }
+                    if(sItem.project.toLowerCase().indexOf(val) > -1){
+                        gen_search_data[sKey] = sItem;
+                        continue;
+                    }
+                    if(sItem.gen_date.indexOf(val) > -1){
+                        gen_search_data[sKey] = sItem;
+                    }
+                }
+                self.vouchersData = gen_search_data;
+            }else{
+                self.vouchersData = func.sortObj(self.orgData);
+            }
+        },
+        pagination: function (self, val) {
+            let rKeys = Object.keys(val);
+            self.pagData = {};
+            self.totRows = rKeys.length;
+            let end = Math.min(self.start + self.maxRows - 1, self.totRows);
+
+            self.calculatePagNo(self);
+
+            if(self.totRows > 0){
+                for(let i=0; i<end; i++){
+                    self.pagData[rKeys[i]] = val[rKeys[i]];
+                }
+            }
+        },
+        calculatePagNo: function (self) {
+            self.totPages = 1;
+            self.curPage = 1;
+            if(self.totRows > self.maxRows){
+                self.totPages = Math.ceil(self.totRows/self.maxRows);
+            }
+        },
+        nextPage: function () {
+            this.curPage += 1;
+        },
+        prevPage: function () {
+            this.curPage -= 1;
+        },
+        changePage: function () {
+            let self = this;
+            let rKeys = Object.keys(self.vouchersData);
+            self.pagData = {};
+            self.totRows = rKeys.length;
+            let end = Math.min(self.start + self.maxRows - 1, self.totRows);
+
+            if(self.totRows > 0){
+                for(let i=(self.start-1); i<end; i++){
+                    self.pagData[rKeys[i]] = self.vouchersData[rKeys[i]];
+                }
+            }
+
         }
     }
 }
